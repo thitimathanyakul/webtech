806d9648a176090386fd0be622ec6cb72305a32f
thitimathanyakul <76036887+thitimathanyakul@users.noreply.github.com>
2020-12-15 17:16:53 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -10,7 +10,8 @@
         <h2>Web Technology for Data Science</h2>
         <p>ชื่อหลักสูตรภาษาไทย : หลักสูตรวิทยาศาสตรบัณฑิต สาขาวิชาวิทยาการข้อมูล</p>
         <p>ภาษาอังกฤษ : Bachelor of Science Program in Data Science</p>
-        <a href="https://www.cp.su.ac.th/curricula/Bachelor-of-Science-Program-in-Data-Science">รายละเอียดหลักสูตร วิทยาการข้อมูล</a>
+        <a href="https://www.cp.su.ac.th/curricula/Bachelor-of-Science-Program-in-Data-Science">รายละเอียดหลักสูตร วิทยาการข้อมูล
+        <img src="emma.png" width="200" height="240"></a>
 
     </body>
 </html>
