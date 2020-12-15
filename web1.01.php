7e4308ab93b3c1b31b53e2a7e3e9bb52cc1eaaa4
thitimathanyakul <76036887+thitimathanyakul@users.noreply.github.com>
2020-12-15 16:38:43 +0800

    Add files via upload

---
+++ web1.01.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<html lang="th">
+    <head>
+        <meta charset="UTF-8">
+        <title>ทอสอบ</title>
+    </head>
+    <body>
+        <h1>สวัสดี ศิลปากร</h1>
+        
+    </body>
+</html>
