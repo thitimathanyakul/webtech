55ab6d7f10385ef5ea3b0078ed2a3311531edee9
thitimathanyakul <76036887+thitimathanyakul@users.noreply.github.com>
2020-12-21 14:54:04 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -2,16 +2,38 @@
 
 <html lang="th">
     <head>
+        <style>
+            h1 {text-align: center;}
+            h2 {text-align: center;}
+            h3 {text-align: center;}
+            li {text-align: center;}
+            img {
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                }
+        </style>
         <meta charset="UTF-8">
-        <title>ทอสอบ</title>
+        <title>620710159</title>
     </head>
     <body>
-        <h1>สวัสดี ศิลปากร</h1>
-        <h2>Web Technology for Data Science</h2>
-        <p>ชื่อหลักสูตรภาษาไทย : หลักสูตรวิทยาศาสตรบัณฑิต สาขาวิชาวิทยาการข้อมูล</p>
-        <p>ภาษาอังกฤษ : Bachelor of Science Program in Data Science</p>
-        <a href="https://www.cp.su.ac.th/curricula/Bachelor-of-Science-Program-in-Data-Science">รายละเอียดหลักสูตร วิทยาการข้อมูล
-        <img src="https://wallpaperaccess.com/full/1082211.jpg" width="200" height="210"></a>
+        <h1 style="color:Blue;border: 2px solid black;"><b>"Nat Thitimathanyakul"</b></h1>
+        <img src="https://scontent.fbkk12-2.fna.fbcdn.net/v/t1.15752-9/131581413_450042149712824_743369365698914889_n.jpg?_nc_cat=104&ccb=2&_nc_sid=ae9488&_nc_eui2=AeGTFr1SiN7kD12flk3Ua4zXi3RiUXS9hM6LdGJRdL2Ezq6MwayfombLKxtRFjq-AYpWxsV0HfU6m6Nf3HvMNebD&_nc_ohc=vfHzk_p1LAcAX-MhOr5&_nc_ht=scontent.fbkk12-2.fna&oh=21caff1680e96f0daf541f9b2618c7fa&oe=6004977B" width="350" height="340">
+        
+        <h2><u>My Nickname is Jam.</u></h2>
+        <h3 style="color:MediumSeaGreen;">I am from Kanchanaburi.</h3>
+        <h3 style="color:MediumSeaGreen;">Birth date : 16/04/2544</h3>
+        <h3 style="color:MediumSeaGreen;">favorite color : Red,Blue</h3>
+        <h3>Hobby :</h3>
+        <ul >
+            <li ><i>Playing game</i></li>
+            <img src="https://learningworksforkids.com/wp-content/uploads/8382505534_83fe421c15_z-640x462.jpg" width="250" height="210">
+            <li><i>Watch a movie</i></li>
+            <img src="https://sm.ign.com/ign_ap/screenshot/default/thibault-penin-3hinbcmq8ro-unsplash_cp4j.jpg" width="" height="210"></a>
+            <li><i>Sleep/Sleepy</i></li>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThTTpvuf6zHzxuZG0847wL_xDUHI9h00cing&usqp=CAU" width="250" height="210">
+        </ul>  
 
+        
     </body>
 </html>
